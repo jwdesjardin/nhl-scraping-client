57b870d28131f32696e696de215060f2c882d8dc
joey desjardins <jwdesjardin@gmail.com>
2021-01-31 12:39:12 -0800

    sort by stat in playerScoring

--- client/src/components/PlayerStats.js
+++ client/src/components/PlayerStats.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import utilStyles from '../styles/utils.module.css';
 import { useSelector } from 'react-redux';
 
@@ -6,33 +6,104 @@ const PlayerStats = () => {
 	const playersScoring = useSelector(state => state.playersScoring);
 	const { data, loading, error } = playersScoring;
 
+	const [ sortProp, setSortProp ] = useState('');
+
+	console.log(sortProp, data);
 	return (
 		<div className={utilStyles.tableContainer}>
 			<table className={utilStyles.table}>
 				<thead>
 					<tr>
-						<th>Player</th>
-						<th>AGE</th>
-						<th>TEAM</th>
-						<th>POS</th>
-						<th>GP</th>
-						<th>G</th>
-						<th>A</th>
-						<th>PTS</th>
-						<th>+/-</th>
-						<th>PIM</th>
-						<th>PSH</th>
-						<th>EV</th>
-						<th>SG</th>
-						<th>PP</th>
-						<th>GW</th>
-						<th>TOI</th>
-						<th>A_TOI</th>
+						<th onClick={e => setSortProp('name')} className={sortProp === 'name' ? 'active' : ''}>
+							Player
+						</th>
+						<th onClick={e => setSortProp('age')} className={sortProp === 'age' ? 'active' : ''}>
+							AGE
+						</th>
+						<th onClick={e => setSortProp('team')} className={sortProp === 'team' ? 'active' : ''}>
+							TEAM
+						</th>
+						<th
+							onClick={e => setSortProp('position')}
+							className={sortProp === 'position' ? 'active' : ''}
+						>
+							POS
+						</th>
+						<th
+							onClick={e => setSortProp('gamesPlayed')}
+							className={sortProp === 'gamesPlayed' ? 'active' : ''}
+						>
+							GP
+						</th>
+						<th
+							onClick={e => setSortProp('goals')}
+							className={sortProp === 'goals' ? 'active' : ''}
+						>
+							G
+						</th>
+						<th
+							onClick={e => setSortProp('assists')}
+							className={sortProp === 'assists' ? 'active' : ''}
+						>
+							A
+						</th>
+						<th
+							onClick={e => setSortProp('points')}
+							className={sortProp === 'points' ? 'active' : ''}
+						>
+							PTS
+						</th>
+						<th
+							onClick={e => setSortProp('plusminus')}
+							className={sortProp === 'plusminus' ? 'active' : ''}
+						>
+							+/-
+						</th>
+						<th onClick={e => setSortProp('pims')} className={sortProp === 'pims' ? 'active' : ''}>
+							PIM
+						</th>
+						<th
+							onClick={e => setSortProp('pointShares')}
+							className={sortProp === 'pointShares' ? 'active' : ''}
+						>
+							PSH
+						</th>
+						<th
+							onClick={e => setSortProp('ev_goals')}
+							className={sortProp === 'ev_goals' ? 'active' : ''}
+						>
+							EV
+						</th>
+						<th
+							onClick={e => setSortProp('sh_goals')}
+							className={sortProp === 'sh_goals' ? 'active' : ''}
+						>
+							SG
+						</th>
+						<th
+							onClick={e => setSortProp('pp_goals')}
+							className={sortProp === 'pp_goals' ? 'active' : ''}
+						>
+							PP
+						</th>
+						<th
+							onClick={e => setSortProp('gw_goals')}
+							className={sortProp === 'gw_goals' ? 'active' : ''}
+						>
+							GW
+						</th>
+						<th onClick={e => setSortProp('toi')} className={sortProp === 'toi' ? 'active' : ''}>
+							TOI
+						</th>
+						<th onClick={e => setSortProp('atoi')} className={sortProp === 'atoi' ? 'active' : ''}>
+							A_TOI
+						</th>
 					</tr>
 				</thead>
 				<tbody>
-					{data.length > 0 &&
-						data.map(player => (
+					{data &&
+						data.length > 0 &&
+						data.sort((a, b) => b[sortProp] - a[sortProp]).map(player => (
 							<tr key={player._id}>
 								<td data_id='name'>{player.name}</td>
 								<td>{player.age}</td>
